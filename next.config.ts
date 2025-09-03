28094e63b79c93e584e9b47d05e73e11aa43d8aa
Alexander Kjærbæk Petersen <kjarbak@Kjrbk-7.local>
2025-09-03 13:45:05 +0200

    setup static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: { unoptimized: true },
+
+  // Fordi GitHub Pages hoster under /portfolio (dit reponavn):
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio",
 };
 
 export default nextConfig;
