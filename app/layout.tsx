fc3f8d56beebc7e2333eb51b31c910cddc01c037
akp0303 <akp0303@gmail.com>
2025-09-15 19:27:03 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,14 @@
-import type { Metadata } from "next";
 import "./globals.css";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Portfolio",
-  description: "Mit site",
+  title: "Alexander Kjærbæk",
+  description: "Portfolio",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="da">
       <body>{children}</body>
     </html>
   );
